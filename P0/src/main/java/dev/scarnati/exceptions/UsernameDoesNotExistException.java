05b8f86a91f1afed5946c588fdf5be99d9bc557b
James Scarnati <89417758+Xerast@users.noreply.github.com>
2021-09-13 14:25:43 -0500

    Delete P0 directory

--- P0/src/main/java/dev/scarnati/exceptions/UsernameDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package dev.scarnati.exceptions;
-
-public class UsernameDoesNotExistException extends Exception {
-    public UsernameDoesNotExistException(String errorMessage) {
-        super(errorMessage);
-    }
-}
